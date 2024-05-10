9b4b8852e01534d8fd46ed08b9c94d8df408f9f3
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-05-10 18:02:58 -0300

    Aulas de Python

---
+++ aula004/aula04.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+print('7'+'4') #74
+print(7+4) #11
+
+# nome = 'Samuel'
+# idade = 14
+# peso = 43
+
+# print(nome,idade,peso)
+
+nome = input('Qual o seu nome?')
+idade = input('Qual sua idade?')
+peso = input('Qual o seu peso?')
+print('Nome:',nome,',Idade:',idade,',Peso:',peso)
